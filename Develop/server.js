a4b4becee0d2c9ffc95043cb4ccc656432b4507b
Snow Day <jakedoty@gmail.com>
2021-02-03 21:16:34 -0600

    heroku start and dynamic port

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,9 @@ var notes = [];
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
   });
