9f675e7fe7bf1eba7805cf5ba54e1ddf43e48ed8
Snow Day <jakedoty@gmail.com>
2021-02-03 20:31:55 -0600

    added api routes

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,8 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var notes = [];
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
@@ -17,4 +19,21 @@ app.get("/notes", function(req, res) {
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+// display all notes
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+  });
+  
+// new notes - takes in JSON input
+app.post("/api/notes", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    var newNote = req.body;
+  
+    console.log(newNote);
+  
+    notes.push(newNote);
+  
+    res.json(newNote);
   });
