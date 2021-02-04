c40d112c1993e588c0b2a94247162e508bc8de58
Snow Day <jakedoty@gmail.com>
2021-02-03 20:19:59 -0600

    added html routes

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,14 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
